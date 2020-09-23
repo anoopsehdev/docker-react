167a89f65ab582f64ce3705a40895d90c8368c27
Anoop Sehdev <anoopsehdev@gmail.com>
2020-09-23 14:02:48 +0100

    New Local Repo

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('1. renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('2. renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
